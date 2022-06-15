33fb8dd3a7c6994b811fff495bc2579d78080e8c
Marco Haber <marcoshaber99@gmail.com>
2022-06-15 22:45:30 +0300

    final

--- script.js
+++ script.js
@@ -1,93 +1,78 @@
-
 const choices = ["rock", "paper", "scissors"];
-
-//everytime there is a winner this array will get filled w/ it.
 let winners = [];
 
-function game() { 
-//5 rounds of the game, with for loop
-for(let i=0; i<5; i++ ) { 
+function game() {
+  for (let i = 1; i <= 5; i++) {
     playRound(i);
   }
-
-  document.querySelector('button').textContent = "Play New Game"
-
-  logTheWinner();
-}
-  
-
-
-function playRound(round) { 
-const playerSelection = playerChoice();
-const computerSelection = computerChoice();
-const winner = checkWinner(playerSelection, computerSelection);
-winners.push(winner);
-resultsOfEachRound(playerSelection, computerSelection, winner,round)
+  document.querySelector("button").textContent = "Play new game";
+  logWins();
 }
 
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+}
 
-//player inputs choice
-function playerChoice() 
-{ 
-    let input = prompt("SELECT FROM: Rock, Paper, & Scissors");
-    while(input == null) { 
-      input =   prompt("SELECT FROM: Rock, Paper, & Scissors");
+function playerChoice() {
+  let input = prompt("Type Rock, Paper, or Scissors");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
+    );
+    while (input == null) {
+      input = prompt("Type Rock, Paper, or Scissors");
     }
     input = input.toLowerCase();
-
-  let check = inputValidation(input);
-  while(check==false) { 
-   input =  prompt("Capital letters do not matter but Spelling needs to be correct");
-
-while(input == null) { 
-   input =  prompt("SELECT FROM: Rock, Paper, & Scissors");
-}
-input = input.toLowerCase();
-check = inputValidation(input);
-}
-return input;
+    check = validateInput(input);
+  }
+  return input;
 }
 
-
-function computerChoice() { 
-//computer inputs random choice
-return choices[Math.floor(Math.random()*choices.length)];
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function inputValidation(choice) {
-return choices.includes(choice);
+function validateInput(choice) {
+  return choices.includes(choice);
 }
 
-function checkWinner(choiceOfPlayer, choiceOfComputer) { 
-if(choiceOfPlayer===choiceOfComputer) { 
-    return "Tie Game";
-  } else if(
-        (choiceOfPlayer === "rock" && choiceOfComputer === "scissors") ||
-        (choiceOfPlayer === "paper" && choiceOfComputer === "rock") ||
-        (choiceOfPlayer === "scissors" && choiceOfComputer === "paper")
-        ){
-              return "Player";
-        } else { 
-            return "Computer";
-        }
+function checkWinner(choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    return "Tie";
+  } else if (
+    (choiceP === "rock" && choiceC == "scissors") ||
+    (choiceP === "paper" && choiceC == "rock") ||
+    (choiceP === "scissors" && choiceC == "paper")
+  ) {
+    return "Player";
+  } else {
+    return "Computer";
+  }
 }
 
-function logTheWinner() { 
-let playerVictories = winners.filter((item) => item == "Player").length;
-let computerVictories = winners.filter((item) => item == "Computer").length;
-let ties = winners.filter((item) => item == "Tie").length;
-console.log("Results: ")
-console.log("Player Victories: ", playerVictories );
-console.log("Computer Victories: ", computerVictories);
-console.log("Ties:", ties);
+function logWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+  console.log("Results:");
+  console.log("Player Wins:", playerWins);
+  console.log("Computer Wins:", computerWins);
+  console.log("Ties:", ties);
 }
 
-function resultsOfEachRound(playerChoice, computerChoice, winner,round) { 
-  console.log("Round: ", round);
-console.log("Player chose: ", playerChoice);
-console.log("Computer Chose: ", computerChoice);
-console.log("Winner Of The Round: ", winner);
-console.log("---------------------")
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round:", round);
+  console.log("Player Chose:", playerChoice);
+  console.log("Computer Chose:", computerChoice);
+  console.log(winner, "Won the Round");
+  console.log("-------------------------------");
 }
-
-
